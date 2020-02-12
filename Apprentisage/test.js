74b1331e445e006d95275976a9bdf43b67849740
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2020-02-12 09:29:51 +0100

    Update test.js

--- Apprentisage/test.js
+++ Apprentisage/test.js
@@ -1,26 +1,14 @@
 var express = require('express');
+var morgan = require('morgan'); // Charge le middleware de logging
+var favicon = require('serve-favicon'); // Charge le middleware de favicon
 
 var app = express();
 
-app.get('/', function(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('Vous êtes à l\'accueil, que puis-je pour vous ?');
-});
-
-app.get('/sous-sol', function(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
-});
-
-app.get('/etage/1/chambre', function(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('Hé ho, c\'est privé ici !');
-});
-// ... Tout le code de gestion des routes (app.get) se trouve au-dessus
-
-app.use(function(req, res, next){
-  res.setHeader('Content-Type', 'text/plain');
-  res.status(404).send('Page introuvable !');
+app.use(morgan('combined')) // Active le middleware de logging
+.use(express.static(__dirname + '/public')) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
+.use(favicon(__dirname + '/public/favicon.ico')) // Active la favicon indiquée
+.use(function(req, res){ // Répond enfin
+    res.send('Hello');
 });
 
 app.listen(8080);
