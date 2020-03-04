54eb1bd45172e166cd058be80d181bd8cc45b2cd
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2020-03-04 17:38:04 +0100

    suite

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended : false}))
 
 app.get('/', (req, res) =>{
     res.render('index.ejs', {name : 'kyle'})
@@ -11,12 +12,16 @@ app.get('/login', (req, res) =>{
     res.render('login.ejs', {name : 'kyle'})
 })
 
+app.post('/login', (req, res) =>{
+    
+} )
+
 app.get('/register', (req, res) =>{
     res.render('register.ejs', {name : 'kyle'})
 })
 
 app.post('/register', (req, res) =>{
-    
+
 } )
 
 app.listen(3000)
