edea48a99f3aa80dfd3b006a5f94205ca8a00484
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-03-04 21:48:49 +0100

    passport suite

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-}
-
-const express = require('express')
-const app = express();
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-
-const initializePassport = require('./passport-config')
-initializePassport(passport, email => users.find(user => user.email === email)
-)
-
-const users = []
-
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({extended : false}))
-app.use(flash())
-app.use(session({
-    secret: process-event.SESSION_SECRET,
+  }
+  
+  const express = require('express')
+  const app = express()
+  const passport = require('passport')
+  const flash = require('express-flash')
+  const session = require('express-session')
+  
+  const initializePassport = require('./passport-config')
+  initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+  )
+  
+  const users = []
+  
+  app.set('view-engine', 'ejs')
+  app.use(express.urlencoded({ extended: false }))
+  app.use(flash())
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
 
-app.get('/', (req, res) =>{
+  app.get('/', (req, res) =>{
     res.render('index.ejs', {name : 'kyle'})
 })
 
@@ -56,6 +59,7 @@ app.post('/register', (req, res) =>{
     } catch (error) {
         res.redirect('/login')
     } 
+    console.log(users)
 } )
-
-app.listen(3000)
+  
+  app.listen(3000)
