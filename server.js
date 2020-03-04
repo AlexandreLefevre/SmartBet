2db089b86f095a652c3abae2e7abbe339a6427ab
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-03-04 21:18:21 +0100

    passport login suite

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express();
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const initializePassport = require('./passport-config')
+initializePassport(passport, email => users.find(user => user.email === email)
+)
+
+const users = []
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({extended : false}))
+app.use(flash())
+app.use(session({
+    secret: process-event.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) =>{
     res.render('index.ejs', {name : 'kyle'})
@@ -12,16 +33,29 @@ app.get('/login', (req, res) =>{
     res.render('login.ejs', {name : 'kyle'})
 })
 
-app.post('/login', (req, res) =>{
-    
-} )
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
 app.get('/register', (req, res) =>{
     res.render('register.ejs', {name : 'kyle'})
 })
 
 app.post('/register', (req, res) =>{
+    try {
 
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.redirect('/login')
+    } catch (error) {
+        res.redirect('/login')
+    } 
 } )
 
 app.listen(3000)
