8a881aa51c4875469f2a14ea56b7f74dd598629a
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-03-09 22:24:24 +0100

    update

--- riv4lgaming/src/components/headerComponent/header.js
+++ riv4lgaming/src/components/headerComponent/header.js
@@ -4,7 +4,24 @@ class Header extends Component {
   render() {
      return(
 
-      test
+      <header>
+        <div className="logo">
+          LOGO
+        </div>
+        <nav>
+          <ul>
+            <li className="Acceuil">
+              <a href="#">Acceuil</a>
+            </li>
+            <li className="Equipe">
+              <a href="#">Equipe</a>
+            </li>
+            <li className="Contact">
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
      );
   }
--- riv4lgaming/src/components/pages/homePage.js
+++ riv4lgaming/src/components/pages/homePage.js
@@ -4,25 +4,7 @@ class Homepage extends Component {
   render() {
      return (
 <main>
-  <article>
-   <aside>
-      <h1>
-        Aside 1
-      </h1>
-   </aside>
-   <section>
-    <h1>
-      Section header
-    </h1>
-    <p>
-      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>  
-   </section>
-    <aside>
-      <h1>
-        Aside 2
-      </h1>
-    </aside>
-  </article>
+  yo
 </main>
      );
   }
