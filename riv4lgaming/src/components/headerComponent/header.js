fe1b160f346acfacd166687f707e345ea52c69b3
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2020-03-10 00:28:36 +0100

    Update header.js

--- riv4lgaming/src/components/headerComponent/header.js
+++ riv4lgaming/src/components/headerComponent/header.js
@@ -3,38 +3,15 @@ import React,  {Component} from 'react';
 class Header extends Component {
   render() {
      return(
-<<<<<<< HEAD
-
-      <header>
-        <div className="logo">
-          LOGO
-        </div>
-        <nav>
-          <ul>
-            <li className="Acceuil">
-              <a href="#">Acceuil</a>
-            </li>
-            <li className="Equipe">
-              <a href="#">Equipe</a>
-            </li>
-            <li className="Contact">
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-=======
-<header>
-<div>
-<h1>Simply The Best</h1>
-<h3>Reasons for Choosing US</h3>
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
-<br></br>
-	<button>READ MORE</button>
-</div>
-</header>
->>>>>>> 484f6cce35f7fb73bd0c712455281255753ed52e
+    <header>
+    <div>
+    <h1>Simply The Best</h1>
+    <h3>Reasons for Choosing US</h3>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
+    <br></br>
+    <button>READ MORE</button>
+    </div>
+    </header>
      );
   }
 }
