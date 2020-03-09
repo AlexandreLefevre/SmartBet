aefbec622fb78a860535e0182055ea4f44aab073
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-03-09 21:18:30 +0100

    Update header.js

--- riv4lgaming/src/components/headerComponent/header.js
+++ riv4lgaming/src/components/headerComponent/header.js
@@ -5,11 +5,14 @@ class Header extends Component {
      return(
 <header>
 <div class="overlay">
-<h1>Simply The Best</h1>
+<h1>RIV4L GAMING</h1>
 <h3>Reasons for Choosing US</h3>
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
 <br></br>
-	<button>READ MORE</button>
+	<button>ACCEUIL</button>  
+  <button>EQUIPE</button>  
+  <button>CONTACT</button>  
+  <button>LOGIN / REGISTER</button>
 </div>
 </header>
      );
