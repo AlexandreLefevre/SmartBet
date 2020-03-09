ad8717c88da1f68bb2dca3d22881ff29838d0243
Cyrton <tongrescyrilpro@gmail.com>
2020-03-09 21:50:24 +0100

    Update homePage.js

--- riv4lgaming/src/components/pages/homePage.js
+++ riv4lgaming/src/components/pages/homePage.js
@@ -3,12 +3,27 @@ import React,  {Component} from 'react';
 class Homepage extends Component {
   render() {
      return (
-       
-        <div className="container-fluid">
-
-            Home Page Content Here !!! 
-
-        </div>
+<main>
+  <article>
+   <aside>
+      <h1>
+        Aside 1
+      </h1>
+   </aside>
+   <section>
+    <h1>
+      Section header
+    </h1>
+    <p>
+      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>  
+   </section>
+    <aside>
+      <h1>
+        Aside 2
+      </h1>
+    </aside>
+  </article>
+</main>
      );
   }
 }
