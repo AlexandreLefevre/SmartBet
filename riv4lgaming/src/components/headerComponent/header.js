a867896de690f9c007781a8e37572a935d001048
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-03-09 20:54:21 +0100

    header

--- riv4lgaming/src/components/headerComponent/header.js
+++ riv4lgaming/src/components/headerComponent/header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
      return(
 <header>
-  <link rel="stylesheet" href="C:\Users\Admin\Documents\GitHub\SmartBet\riv4lgaming\src\components\headerComponent\header.css"></link>
+  <link rel="stylesheet" href='./src/Assets/CSS/header.css'></link>
 <div class="overlay">
 <h1>Simply The Best</h1>
 <h3>Reasons for Choosing US</h3>
