f8f0ed56d11a39033491dbcddbe2e9871183ac2e
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-03-11 10:45:41 +0100

    Update loginPage.js

--- riv4lgaming/src/components/pages/loginPage.js
+++ riv4lgaming/src/components/pages/loginPage.js
@@ -6,15 +6,15 @@ class LoginPage extends Component {
       <body class="loginbody">
       <div className="container-fluid">
        
-      <h1 class="titre">Connection</h1>
+      <h1 class="titre">Connexion</h1>
  
  <form>
    <div class="container">
-     <p>Connection</p>
+     <p>Connexion</p>
      <p>Pseudo</p>
-     <input type="text" placeholder="username" name="username" required></input>
+     <input type="text" placeholder="Pseudo" name="username" required></input>
      <p>Mot de passe</p>
-     <input type="password" placeholder="Enter Password" name="psw" required></input>
+     <input type="password" placeholder="Entrer mot de passe" name="psw" required></input>
      <p>Enregistrer un nouveau membre :  <a href="/register" >Enregistrement</a>.</p>
  
      </div>
