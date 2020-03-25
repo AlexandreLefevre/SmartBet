fcb50e5a1b289fe4f7ff45e51da6b6f8d9581c70
Tongres <tongrescyril26@gmail.com>
2020-03-25 17:57:49 +0100

    nodemailer
    envoie d'email

--- riv4lgaming/Server/src/index.js
+++ riv4lgaming/Server/src/index.js
@@ -4,6 +4,8 @@ const UserModel = require('./user.model')
 const express = require('express')
 const app = express()
 const port = 4000
+const nodemailer = require('nodemailer')
+const logingmail = require('./config')
 
 mongoose.set('useFindAndModify', false)
 mongoose.set('useCreateIndex', true)
@@ -35,6 +37,7 @@ app.use((req, res, next) => {
 
   next()
 })
+
 db.once('open', async () => {
 
   app.get('/', (req, res) => res.send('Hello World!'))
@@ -50,16 +53,79 @@ db.once('open', async () => {
 
     await myUser.save()
     res.send('Account created')
-
   })
 
   app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-
-
 })
 
+let transporter = nodemailer.createTransport({ //port ?? nodemailer.createTransport(transport[port, defaults])
+  service: 'gmail',
+  auth: {
+    user: logingmail.user,
+    pass: logingmail.pass,
+  }
+});
+
+var message = {
+    from: "riv4lgaming.be@gmail.com",
+    to: "tongrescyril26@gmail.com",
+    subject: "[R4G] Confirmation d'inscription",
+    text: "Plaintext version of the message",
+    html: "<p>HTML version of the message</p>"
+  };
+
+transporter.sendMail(message, function(a, b){
+  console.log(a);
+  console.log(b);
+});
+
+// verify connection configuration
+transporter.verify(function(error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+
+/*
+transporter.sendMail(data[port, callback]) //port ??
+var message = {
+  from: "riv4lgaming.be@gmail.com",
+  to: "receiver@sender.com",
+  subject: "[R4G] Confirmation d'inscription",
+  text: "Plaintext version of the message",
+  html: "<p>HTML version of the message</p>"
+};*/
+
+//nodemailer
+/*
+pp.post('/send', function(req, res, next) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'test-email@gmail.com',
+      pass: 'test123'
+    }
+  })
+}
+  const mailOptions = {
+    from: `${req.body.email}`,
+    to: 'test-email@gmail.com',
+    subject: `${req.body.name}`,
+    text: `${req.body.message}`,
+    replyTo: `${req.body.email}`
+  }
+  transporter.sendMail(mailOptions, function(err, res) {
+    if (err) {
+      console.error('there was an error: ', err);
+    } else {
+      console.log('here is the res: ', res)
+    }
+  })
+})
+*/
 //
 // /*CREATE USER */
 // const myUser = new UserModel({
