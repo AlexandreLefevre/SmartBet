e0635245f793fa0eaf88a6b2c9d0d9283d982aaa
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-04-22 11:20:11 +0200

    lien discord

--- riv4lgaming/front/src/components/pages/commu.js
+++ riv4lgaming/front/src/components/pages/commu.js
@@ -1,4 +1,7 @@
 import React,  {Component} from 'react';
+import { Row, Col, Typography  } from 'antd'
+
+const style = {padding: '8px 30px'};
 
 class Forum extends Component {
   render() {
@@ -6,6 +9,15 @@ class Forum extends Component {
       <body>
         <h1>Communauté</h1>
           <p>Comment ça marche et comment nous rejoindre ? </p>
+          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+      <Col className="gutter-row" span={6}>
+        <div style={style}>
+          <a href="https://discord.gg/uY9gTF">
+              < img src="https://github.com/GillesTimothy/timot/blob/master/discord-1200x630.png?raw=true" alt="sc2" style={{width:"130%", height:"130%"}}></img>
+          </a>
+        </div>
+      </Col>
+      </Row>
       </body>
      );
   }
