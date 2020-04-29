5b074f356d2389affa826ab495e37f9c79583a55
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2020-04-30 00:29:38 +0200

    suite comu

--- riv4lgaming/front/src/components/pages/commu.js
+++ riv4lgaming/front/src/components/pages/commu.js
@@ -2,6 +2,7 @@ import React,  {Component} from 'react';
 import { Row, Col, Typography  } from 'antd'
 
 const style = {padding: '8px 30px'};
+const { Title } = Typography;
 
 class Forum extends Component {
   render() {
@@ -9,15 +10,16 @@ class Forum extends Component {
       <body>
          <Row>
           <Col flex="100px"></Col>
-        <h1>Communauté</h1>
-        </Row>
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+          <Title level={1} >Communauté</Title>
+        <Col flex="auto">
+        <Title level={2} >1) Créez un compte discord</Title>
       <Col className="gutter-row" span={6}>
         <div style={style}>
           <a href="https://discord.gg/uY9gTF">
               < img src="https://github.com/GillesTimothy/timot/blob/master/discord-1200x630.png?raw=true" alt="sc2" style={{width:"130%", height:"130%"}}></img>
           </a>
         </div>
+        </Col>
       </Col>
       </Row>
       </body>
