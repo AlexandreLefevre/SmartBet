f0c171ecdbe83954b866d08df8916ab8fd3d870e
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2020-04-30 00:05:31 +0200

    debut communauté

--- riv4lgaming/front/src/components/pages/commu.js
+++ riv4lgaming/front/src/components/pages/commu.js
@@ -7,8 +7,10 @@ class Forum extends Component {
   render() {
      return (
       <body>
+        <div style={{textAlign: "center"}}>
         <h1>Communauté</h1>
           <p>Comment ça marche et comment nous rejoindre ? </p>
+          </div>
           <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
       <Col className="gutter-row" span={6}>
         <div style={style}>
