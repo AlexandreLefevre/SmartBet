150756c3f2828bd62d52fbf031ce865f66778cd9
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2020-04-30 01:08:12 +0200

    page commu suite

--- riv4lgaming/front/src/components/pages/commu.js
+++ riv4lgaming/front/src/components/pages/commu.js
@@ -1,29 +1,32 @@
 import React,  {Component} from 'react';
 import { Row, Col, Typography  } from 'antd'
 
-
 const style = {padding: '8px 30px'};
 const { Title } = Typography;
 
 class Forum extends Component {
   render() {
      return (
-       <body>
+      <body>
       <div style={{textAlign: "center"}}>
          <Title level={1} >Communauté</Title>
          </div>
         <Row>
           <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-            Creez un compte discord
+          <Title level={3} >Créez un compte discord</Title>
           </Col>
           <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-            Rejoignez le serveur discord
-          </Col>
+          <Title level={3} >Rejoignez le serveur discord</Title>
+          <a href="https://discord.gg/uY9gTF">
+              < img src="https://github.com/GillesTimothy/timot/blob/master/discord-1200x630.png?raw=true" alt="sc2" style={{width:"80%", height:"80%"}}></img>
+          </a>
+        </Col>
           <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-            Présentez vous 
+          <Title level={3} >Présentez vous </Title>
           </Col>
         </Row>
         </body>
+
      );
   }
 }
