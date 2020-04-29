0581e25f734af7436b9a278419a27aa2b2932c75
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2020-04-30 00:55:10 +0200

    omg

--- riv4lgaming/front/src/components/pages/commu.js
+++ riv4lgaming/front/src/components/pages/commu.js
@@ -1,28 +1,29 @@
 import React,  {Component} from 'react';
 import { Row, Col, Typography  } from 'antd'
 
+
 const style = {padding: '8px 30px'};
 const { Title } = Typography;
 
 class Forum extends Component {
   render() {
      return (
-      <body>
-         <Row>
-          <Col flex="100px"></Col>
-          <Title level={1} >Communauté</Title>
-        <Col flex="auto">
-        <Title level={2} >1) Créez un compte discord</Title>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>
-          <a href="https://discord.gg/uY9gTF">
-              < img src="https://github.com/GillesTimothy/timot/blob/master/discord-1200x630.png?raw=true" alt="sc2" style={{width:"130%", height:"130%"}}></img>
-          </a>
-        </div>
-        </Col>
-      </Col>
-      </Row>
-      </body>
+       <body>
+      <div style={{textAlign: "center"}}>
+         <Title level={1} >Communauté</Title>
+         </div>
+        <Row>
+          <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            Creez un compte discord
+          </Col>
+          <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            Rejoignez le serveur discord
+          </Col>
+          <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            Présentez vous 
+          </Col>
+        </Row>
+        </body>
      );
   }
 }
