b75765ce5cf5638a27a8fe7bd814782b21b88cac
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2020-04-30 02:00:09 +0200

    historique front end

--- riv4lgaming/front/src/components/pages/histo.js
+++ riv4lgaming/front/src/components/pages/histo.js
@@ -1,20 +1,66 @@
-import React,  {Component} from 'react';
+import React,  {Component} from 'react'
+import {Link} from 'react-router-dom'
+import { Row } from "antd"
+import { Collapse } from 'antd';
+import { Col } from "antd"
+import { Descriptions, Badge } from 'antd';
+
+const { Panel } = Collapse;
+const text = ' Descriptif du tournois ....';
 
 class Histo extends Component {
+
   render() {
      return (
-       <div className="container-fluid">
-
-        <h1>Historique</h1>
-
-        <p>
-            historique
-        </p>
-
-       </div>
-
-     );
+      <body>
+        <Row>
+          <Col flex="100px"></Col>
+          <Col flex="auto">
+          <div>
+          <p>Historique de vos participation aux tournois en fonction des jeux.</p>
+          </div>
+          </Col>
+          </Row>
+        <Row>
+          
+        <Collapse accordion>
+    <Panel header="Starcraf 2" key="1">
+      <p>{text}</p>
+          <Descriptions title="Info Tournois" bordered>
+              <Descriptions.Item label="Organisateur">nom organisateur</Descriptions.Item>
+              <Descriptions.Item label="Gagnant">nom gagnant</Descriptions.Item>
+              <Descriptions.Item label="Nbr particiapnt">nbr</Descriptions.Item>
+              <Descriptions.Item label="Date début">2018-04-24 18:00:00</Descriptions.Item>
+              <Descriptions.Item label="Date fin" span={2}>
+                2019-04-24 18:00:00
+              </Descriptions.Item>
+              <Descriptions.Item label="Status" span={2}>
+                <Badge status="processing" text="Running" />
+              </Descriptions.Item>
+              <Descriptions.Item label="Place">1er</Descriptions.Item>
+          </Descriptions>
+    </Panel>
+    <Panel header="Rocket League" key="2">
+      <p>{text}</p>
+    </Panel>
+    <Panel header="Counter-Strike Global Offensive" key="3">
+      <p>{text}</p>
+    </Panel>
+    <Panel header="League of Legends" key="4">
+      <p>{text}</p>
+    </Panel>
+    <Panel header="Valorant" key="5">
+      <p>{text}</p>
+    </Panel>
+  </Collapse>,  
+          
+        </Row>
+      </body>
+      
+    );
   }
 }
+
+
 
 export default Histo;
