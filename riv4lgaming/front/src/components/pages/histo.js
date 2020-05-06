0ff39c1904b569bf95bc2393c70db6eadc80ea8c
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2020-05-06 14:33:53 +0200

    Update histo.js

--- riv4lgaming/front/src/components/pages/histo.js
+++ riv4lgaming/front/src/components/pages/histo.js
@@ -27,8 +27,8 @@ class Histo extends Component {
     <Panel header="Starcraf 2" key="1">
       <p>{text}</p>
           <Descriptions title="Info Tournois" bordered>
-              <Descriptions.Item label="Organisateur">nom organisateur</Descriptions.Item>
-              <Descriptions.Item label="Gagnant">nom gagnant</Descriptions.Item>
+              <Descriptions.Item label="Organisateur"> Nom Organisateur</Descriptions.Item>
+              <Descriptions.Item label="Gagnant">Nom Gagnant</Descriptions.Item>
               <Descriptions.Item label="Nbr particiapnt">nbr</Descriptions.Item>
               <Descriptions.Item label="Date début">2018-04-24 18:00:00</Descriptions.Item>
               <Descriptions.Item label="Date fin" span={2}>
@@ -42,15 +42,67 @@ class Histo extends Component {
     </Panel>
     <Panel header="Rocket League" key="2">
       <p>{text}</p>
+      <Descriptions title="Info Tournois" bordered>
+              <Descriptions.Item label="Organisateur"> Nom Organisateur</Descriptions.Item>
+              <Descriptions.Item label="Gagnant">Nom Gagnant</Descriptions.Item>
+              <Descriptions.Item label="Nbr particiapnt">nbr</Descriptions.Item>
+              <Descriptions.Item label="Date début">2018-04-24 18:00:00</Descriptions.Item>
+              <Descriptions.Item label="Date fin" span={2}>
+                2019-04-24 18:00:00
+              </Descriptions.Item>
+              <Descriptions.Item label="Status" span={2}>
+                <Badge status="processing" text="Running" />
+              </Descriptions.Item>
+              <Descriptions.Item label="Place">1er</Descriptions.Item>
+          </Descriptions>
     </Panel>
     <Panel header="Counter-Strike Global Offensive" key="3">
       <p>{text}</p>
+      <Descriptions title="Info Tournois" bordered>
+              <Descriptions.Item label="Organisateur"> Nom Organisateur</Descriptions.Item>
+              <Descriptions.Item label="Gagnant">Nom Gagnant</Descriptions.Item>
+              <Descriptions.Item label="Nbr particiapnt">nbr</Descriptions.Item>
+              <Descriptions.Item label="Date début">2018-04-24 18:00:00</Descriptions.Item>
+              <Descriptions.Item label="Date fin" span={2}>
+                2019-04-24 18:00:00
+              </Descriptions.Item>
+              <Descriptions.Item label="Status" span={2}>
+                <Badge status="processing" text="Running" />
+              </Descriptions.Item>
+              <Descriptions.Item label="Place">1er</Descriptions.Item>
+          </Descriptions>
     </Panel>
     <Panel header="League of Legends" key="4">
       <p>{text}</p>
+      <Descriptions title="Info Tournois" bordered>
+              <Descriptions.Item label="Organisateur"> Nom Organisateur</Descriptions.Item>
+              <Descriptions.Item label="Gagnant">Nom Gagnant</Descriptions.Item>
+              <Descriptions.Item label="Nbr particiapnt">nbr</Descriptions.Item>
+              <Descriptions.Item label="Date début">2018-04-24 18:00:00</Descriptions.Item>
+              <Descriptions.Item label="Date fin" span={2}>
+                2019-04-24 18:00:00
+              </Descriptions.Item>
+              <Descriptions.Item label="Status" span={2}>
+                <Badge status="processing" text="Running" />
+              </Descriptions.Item>
+              <Descriptions.Item label="Place">1er</Descriptions.Item>
+          </Descriptions>
     </Panel>
     <Panel header="Valorant" key="5">
       <p>{text}</p>
+      <Descriptions title="Info Tournois" bordered>
+              <Descriptions.Item label="Organisateur"> Nom Organisateur</Descriptions.Item>
+              <Descriptions.Item label="Gagnant">Nom Gagnant</Descriptions.Item>
+              <Descriptions.Item label="Nbr particiapnt">nbr</Descriptions.Item>
+              <Descriptions.Item label="Date début">2018-04-24 18:00:00</Descriptions.Item>
+              <Descriptions.Item label="Date fin" span={2}>
+                2019-04-24 18:00:00
+              </Descriptions.Item>
+              <Descriptions.Item label="Status" span={2}>
+                <Badge status="processing" text="Running" />
+              </Descriptions.Item>
+              <Descriptions.Item label="Place">1er</Descriptions.Item>
+          </Descriptions>
     </Panel>
   </Collapse>,  
           
