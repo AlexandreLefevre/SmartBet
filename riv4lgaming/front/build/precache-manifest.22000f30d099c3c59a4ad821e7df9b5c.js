09dc7ac60eba71c434eb40cc9e324d3f8192c847
Tongres <tongrescyril26@gmail.com>
2020-05-26 19:10:48 +0200

    last

---
+++ riv4lgaming/front/build/precache-manifest.22000f30d099c3c59a4ad821e7df9b5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d78a7f0073bef68f403fa60dab7087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d8e11ee49a985d8effc",
+    "url": "/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "cd4dfaeee885605261fe",
+    "url": "/static/css/main.bc99ce7a.chunk.css"
+  },
+  {
+    "revision": "2d8e11ee49a985d8effc",
+    "url": "/static/js/2.9109b6f5.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/static/js/2.9109b6f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4dfaeee885605261fe",
+    "url": "/static/js/main.9bb26383.chunk.js"
+  },
+  {
+    "revision": "ecd740789e2d45005734",
+    "url": "/static/js/runtime-main.7c2592d7.js"
+  }
+]);
