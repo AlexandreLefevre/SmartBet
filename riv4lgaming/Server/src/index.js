6a5c57977b0420ea1e0108f906b32c295a7f3cb8
Tongres <tongrescyril26@gmail.com>
2020-05-26 13:06:06 +0200

    ok

--- riv4lgaming/Server/src/index.js
+++ riv4lgaming/Server/src/index.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser')
 mongoose.set('useFindAndModify', false)
 mongoose.set('useCreateIndex', true)
 
-  mongoose.connection.openUri(`mongodb+srv://riv4lgaming:<password>@cluster0-kkjt4.mongodb.net/test?retryWrites=true&w=majority`, {
+  mongoose.connection.openUri(`mongodb+srv://riv4lgaming:riv4lG24@cluster0-kkjt4.mongodb.net/test?retryWrites=true&w=majority`, {
     keepAlive: true,
     reconnectTries: Number.MAX_VALUE,
     useNewUrlParser: true,
